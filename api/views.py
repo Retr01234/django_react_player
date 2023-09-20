e6c2375b4f8aca90df852f039e64970b6e3351e9
Pave <pitcheroty@gmail.com>
2023-09-20 20:31:58 +0200

    feature: add serializer endpoint to access groups

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import GroupSerializer
+from .models import Group
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Start Page")
+class GroupView(generics.CreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
