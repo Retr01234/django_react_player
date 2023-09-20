764e0cacf4e80c016ae18ca240f7426c17df9a58
Pave <pitcheroty@gmail.com>
2023-09-20 20:17:34 +0200

    feature: create group db model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    identifier = models.CharField(max_length=5, default="", unique=True)
+    owner = models.CharField(max_length=75, unique=True)
+    pausible = models.BooleanField(default= False, null=False)
+    skippable = models.IntegerField(null=False, default=1)
+    created = models.DateTimeField(auto_now_add=True)
